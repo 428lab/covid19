f793e6e38b2ea227780b7ae106499584ac329c82
yamamoto_shinya <y@zenryokukikai.com>
2021-02-05 23:30:12 +0900

    The first commit.

---
+++ covid19.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+covid19 = True
+while(covid19):
+    for you in everyone:
+        stay_at_home += stayAtHome(you)
+        wash_hands += washHands(you)
+        social_distancing += maintainSocialDistancing(you)
+
+    coivd19 = stillCoivd19(stay_at_home, wash_hands, social_distancing)
+
+takeOffTheMask(everyone)
+letsGoOutForSomeDrinks(everyone)
+
